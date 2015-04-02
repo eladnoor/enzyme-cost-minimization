18a12052e0f1df50d813dabbbb950b4c3c12fbce
Elad Noor <elad.noor@gmail.com>
2015-04-02 15:20:56 +0200

    using data from Karl instead of original flux and proteomic data that Wolf was using

--- matlab/enzyme-cost-minimization/demo/correlate_loglog.m
+++ matlab/enzyme-cost-minimization/demo/correlate_loglog.m
@@ -1,21 +1,24 @@
-function correlate_loglog(x, y, labels, plot_title)
-if nargin < 4
-    plot_title = 'correlation';
+function correlate_loglog(x, y, labels)
+if nargin < 3
+    labels = [];
 end
-ind = intersect(find(~isnan(x)), find(~isnan(y)));
+
+% use only indices where both x and y are not NaN and non-zero
+ind = find(~(isnan(x) + isnan(y) + (x == 0) + (y == 0)));
 
 figure;
 hold on;
 plot(x(ind), y(ind), 'o');
 plot([1e-3, 1e-1], [1e-4, 1], 'k:');
-text(x(ind)*1.05, y(ind), labels(ind));
+if ~isempty(labels)
+    text(x(ind)*1.05, y(ind), labels(ind));
+end
 set(gca,'xscale','log');
 set(gca,'yscale','log');
 hold off;
 [r_lin,~] = corrcoef([x(ind), y(ind)]);
 [r_log,~] = corrcoef([log(x(ind)), log(y(ind))]);
 
-title(sprintf(['\\fontsize{14}%s\\fontsize{9}',...
-               '\\newline r (linear): ', ...
-               '%.2f\\newline r (log): %.2f'], ...
-              plot_title, r_lin(1,2), r_log(1,2)));
+title(sprintf(['r (linear): %.2f\\newline', ...
+               'r (log)   : %.2f'], ...
+               r_lin(1,2), r_log(1,2)));
--- matlab/enzyme-cost-minimization/demo/demo_karl.m
+++ matlab/enzyme-cost-minimization/demo/demo_karl.m
@@ -11,13 +11,9 @@
 % The results (in SBtab format) are written to the subdirectory 'results'
 % --------------------------------------------------------
 
-
-% --------------------------------------------------------
-% Set file names
-
-result_DIR = 'results';
-infile_ecm = 'data/ecm_karl.csv';
-%infile_ecm = 'data/ecm_ecoli_aerobic.csv';
+[path, ~, ~] = fileparts(which(mfilename));
+result_path = [path '/results'];
+infile_ecm = [path '/data/ecm_karl.csv'];
 
 % --------------------------------------------------------
 % Run Enzyme Cost Minimization
@@ -25,16 +21,41 @@ infile_ecm = 'data/ecm_karl.csv';
 options = struct('actions','ecm_standard');
 %options = struct('actions','parameter_balancing');
 
-[report, errors] = ecm_simple(infile_ecm, result_DIR, options);
+[report, errors] = ecm_simple(infile_ecm, result_path, options);
 
 %% --------------------------------------------------------
 % Correlate results with measured proteomics
 my_sbtab = sbtab_document_load_from_one(infile_ecm);
-genes = sbtab_table_get_column(my_sbtab.tables.Reaction,'Gene',0);
+model_gene = sbtab_table_get_column(my_sbtab.tables.Reaction,'NameForPlots',0);
+model_bnumber = sbtab_table_get_column(my_sbtab.tables.Reaction,'Bnumber',0);
 meas_reaction_names = sbtab_table_get_column(my_sbtab.tables.EnzymeConcentration,'Reaction',0);
-meas_data = sbtab_table_get_column(my_sbtab.tables.EnzymeConcentration,'EnzymeConcentration',1);
 
-result_sbtab = sbtab_document_load_from_one([result_DIR '/ecm_result.csv_Predictions.csv']);
+proteome_bnumber = sbtab_table_get_column(my_sbtab.tables.Proteomics,'Bnumber',0);
+proteome_abundance = sbtab_table_get_column(my_sbtab.tables.Proteomics,'Abundance',1);
+
+%%
+if false
+    %meas_data = sbtab_table_get_column(my_sbtab.tables.EnzymeConcentration,'EnzymeConcentration',1);
+else
+    meas_data = zeros(size(model_bnumber));
+    for i = 1:length(model_bnumber)
+        bnumbers = strsplit(model_bnumber{i}, ' + ');
+        for j = 1:length(bnumbers)
+            idx = find(ismember(proteome_bnumber, bnumbers{j}));
+            if ~isempty(idx)
+                meas_data(i) = meas_data(i) + proteome_abundance(idx);
+            end
+        end
+    end
+    
+    % convert copies per fl (of cytoplasm) to mM
+    V_cell = 0.66e-3; % [L] for 1L of culture at OD600 = 1
+    N_A = 6.022e23;
+    meas_data = meas_data * (1e15 / N_A) / V_cell;
+end
+
+%%
+result_sbtab = sbtab_document_load_from_one([result_path '/ecm_result.csv_Predictions.csv']);
 pred_reaction_names = sbtab_table_get_column(result_sbtab.tables.Predictedenzymelevels,'Reaction',0);
 pred_headers = result_sbtab.tables.Predictedenzymelevels.uncontrolled.headers;
 pred_data = str2double(result_sbtab.tables.Predictedenzymelevels.uncontrolled.data);
@@ -43,7 +64,9 @@ pred_data = str2double(result_sbtab.tables.Predictedenzymelevels.uncontrolled.da
 assert(all(strcmp(meas_reaction_names, pred_reaction_names)));
 
 %%
+%method = 'ecf3s';
 method = 'ecf3s';
-%method = 'ecf4cmr';
 method_col = find(ismember(pred_headers, method));
-correlate_loglog(meas_data1, pred_data(:, method_col), genes, method);
+correlate_loglog(meas_data, pred_data(:, method_col), model_gene);
+xlabel('Meas. abundance (mM)');
+ylabel(['Pred. abundance (mM): ' method]);
